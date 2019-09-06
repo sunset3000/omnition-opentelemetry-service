31f651b5e1ca0ef6bc5bd4d464169b195c72d9b2
Tigran Najaryan <tigran@omnition.io>
2019-09-06 10:50:04 -0400

    Add ShardingInMemConfig struct

--- exporter/omnitelk/sharding_config.go
+++ exporter/omnitelk/sharding_config.go
@@ -1,1 +1,65 @@
+// Copyright 2019 OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package omnitelk
+
+import (
+	"math/big"
+	"sort"
+
+	omnitelk "github.com/Omnition/omnition-opentelemetry-service/exporter/omnitelk/gen"
+)
+
+// ShardingInMemConfig is an immutable in-memory representation of sharding
+// configuration.
+type ShardingInMemConfig struct {
+	// List of shards sorted by startingHashKey.
+	shards []ShardInMemConfig
+}
+
+// ShardInMemConfig  is an immutable in-memory representation of one shard
+// configuration.
+type ShardInMemConfig struct {
+	shardID         []byte
+	startingHashKey big.Int
+	endingHashKey   big.Int
+}
+
+// byStartingHashKey implements sort.Interface for []ShardInMemConfig based on
+// the startingHashKey field.
+type byStartingHashKey []ShardInMemConfig
+
+func (a byStartingHashKey) Len() int      { return len(a) }
+func (a byStartingHashKey) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a byStartingHashKey) Less(i, j int) bool {
+	return a[i].startingHashKey.Cmp(&a[j].startingHashKey) < 0
+}
+
+// NewShardingInMemConfig creates a ShardingInMemConfig from omnitelk.ShardingConfig.
+func NewShardingInMemConfig(pbConf *omnitelk.ShardingConfig) *ShardingInMemConfig {
+	sc := &ShardingInMemConfig{}
+	for _, s := range pbConf.ShardDefinitions {
+		shard := ShardInMemConfig{
+			shardID: s.ShardId,
+		}
+		shard.startingHashKey.SetBytes(s.StartingHashKey)
+		shard.endingHashKey.SetBytes(s.EndingHashKey)
+
+		sc.shards = append(sc.shards, shard)
+	}
+
+	sort.Sort(byStartingHashKey(sc.shards))
+
+	return sc
+}
--- exporter/omnitelk/sharding_config_test.go
+++ exporter/omnitelk/sharding_config_test.go
@@ -1,1 +1,59 @@
+// Copyright 2019 OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package omnitelk
+
+import (
+	"testing"
+
+	omnitelk "github.com/Omnition/omnition-opentelemetry-service/exporter/omnitelk/gen"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewShardingInMemConfig(t *testing.T) {
+	pbConf := &omnitelk.ShardingConfig{
+		ShardDefinitions: []*omnitelk.ShardDefinition{
+			{
+				ShardId:         []byte{1, 2, 3},
+				StartingHashKey: []byte{4, 5, 6},
+				EndingHashKey:   []byte{7, 8, 9},
+			},
+			{
+				ShardId:         []byte{0, 1, 2},
+				StartingHashKey: []byte{3, 4, 5},
+				EndingHashKey:   []byte{6, 7, 8},
+			},
+		},
+	}
+
+	sc := NewShardingInMemConfig(pbConf)
+
+	want := &ShardingInMemConfig{
+		shards: []ShardInMemConfig{
+			{
+				shardID: []byte{0, 1, 2},
+			},
+			{
+				shardID: []byte{1, 2, 3},
+			},
+		},
+	}
+	want.shards[0].startingHashKey.SetUint64(197637)
+	want.shards[0].endingHashKey.SetUint64(395016)
+
+	want.shards[1].startingHashKey.SetUint64(263430)
+	want.shards[1].endingHashKey.SetUint64(460809)
+
+	assert.Equal(t, sc, want)
+}
