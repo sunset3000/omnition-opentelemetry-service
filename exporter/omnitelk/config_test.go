97368ea3fde8c4b12de717b72c6c8b4d1bb13261
Tigran Najaryan <tigran@omnition.io>
2019-09-06 09:30:13 -0400

    Add OmnitelK exporter
    This adds empty OmnitelK exporter, without implementation.
    The exporter is not yet enabled since the factory is not registered
    during startup, so the code is not reachable.

---
+++ exporter/omnitelk/config_test.go
@@ -0,0 +1,74 @@
+// Copyright 2019 OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package omnitelk
+
+import (
+	"path"
+	"testing"
+
+	"github.com/open-telemetry/opentelemetry-service/config"
+	"github.com/open-telemetry/opentelemetry-service/config/configmodels"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDefaultConfig(t *testing.T) {
+	receiverFactories, processorsFactories, exporterFactories, err := config.ExampleComponents()
+	assert.Nil(t, err)
+
+	factory := &Factory{}
+	exporterFactories[factory.Type()] = factory
+	cfg, err := config.LoadConfigFile(
+		t, path.Join(".", "testdata", "default.yaml"), receiverFactories, processorsFactories, exporterFactories,
+	)
+	require.NoError(t, err)
+	require.NotNil(t, cfg)
+
+	e := cfg.Exporters["omnitelk"]
+
+	assert.Equal(t, e,
+		&Config{
+			ExporterSettings: configmodels.ExporterSettings{
+				TypeVal: "omnitelk",
+				NameVal: "omnitelk",
+			},
+		},
+	)
+}
+
+func TestConfig(t *testing.T) {
+	receiverFactories, processorsFactories, exporterFactories, err := config.ExampleComponents()
+	assert.Nil(t, err)
+
+	factory := &Factory{}
+	exporterFactories[factory.Type()] = factory
+	cfg, err := config.LoadConfigFile(
+		t, path.Join(".", "testdata", "config.yaml"), receiverFactories, processorsFactories, exporterFactories,
+	)
+
+	require.NoError(t, err)
+	require.NotNil(t, cfg)
+
+	e := cfg.Exporters["omnitelk"]
+
+	assert.Equal(t, e,
+		&Config{
+			ExporterSettings: configmodels.ExporterSettings{
+				TypeVal: "omnitelk",
+				NameVal: "omnitelk",
+			},
+		},
+	)
+}
